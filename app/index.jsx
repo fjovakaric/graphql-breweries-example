2586887015f757a5982fcc21eb523859aff18d39
fj <filip.jovakaric@gmail.com>
2016-10-10 15:09:49 +0200

    create basic react-redux homepage #5 - index.jsx refactored

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+
+import configureStore from './configureStore.es6';
+import routes from './routes.jsx';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
+const store = configureStore();
+
 render (
-    <h1>React app</h1>,
+    <Provider store={store} >
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
