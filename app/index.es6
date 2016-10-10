e638591912aabd903a36b8c90be6d2d62ef50cee
fj <filip.jovakaric@gmail.com>
2016-10-10 14:56:34 +0200

    create basic react-redux homepage #5 - index.jsx renamed

--- app/index.es6
+++
@@ -1,1 +0,0 @@
-console.log('webpack working with hot reloading!');
---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+render (
+    <h1>React app</h1>,
+    document.getElementById('app')
+);
