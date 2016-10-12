cc9855c13c822b8307851c8f408d5094524c73aa
fj <filip.jovakaric@gmail.com>
2016-10-12 11:03:49 +0200

    Create Breweries Page #9 - breweries page added to routes

--- app/routes.jsx
+++ app/routes.jsx
@@ -3,12 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App/App.jsx';
 import HomePage from './pages/HomePage/HomePage.jsx';
+import BreweriesPage from './pages/BreweriesPage/BreweriesPage.jsx';
 import GraphiQLPage from './pages/GraphiQLPage/GraphiQLPage.jsx';
 
 export default (
     <Route>
         <Route component={App}>
             <Route path="/" component={HomePage}/>
+            <Route path="/breweries" component={BreweriesPage}/>
         </Route>
 
         <Route path="/graphiql" component={GraphiQLPage}/>
