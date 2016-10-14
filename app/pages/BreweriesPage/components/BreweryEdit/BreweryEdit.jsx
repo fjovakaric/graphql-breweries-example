2b8fdb0784d1b13415ebc2dd04b6c104de1b1fc0
fj <filip.jovakaric@gmail.com>
2016-10-14 20:11:17 +0200

    Create SmartInput component #16 - BreweryEdit updated

--- app/pages/BreweriesPage/components/BreweryEdit/BreweryEdit.jsx
+++ app/pages/BreweriesPage/components/BreweryEdit/BreweryEdit.jsx
@@ -3,63 +3,35 @@ import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import FaFloppyO from 'react-icons/lib/fa/floppy-o';
 import FaTimes from 'react-icons/lib/fa/times-circle-o';
 
+import SmartInput from '../../../../components/SmartInput/SmartInput.jsx';
+
 
 import './_brewery-edit.scss';
 
 // Set Component Name
-class BreweriesTable extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        const emptyBrewery = {
-            name: '',
-            description: '',
-            established: 0,
-            website: '',
-            country: ''
-        };
-
-        this.state = {
-            brewery: Object.assign(emptyBrewery, props.brewery)
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (this.props.brewery._id != nextProps.brewery._id) {
-            this.setState({
-                brewery: nextProps.brewery
-            })
-        }
-    }
-
-    updateField(fieldName, e) {
-        const breweryToEdit =  Object.assign({}, this.state.brewery);
-        breweryToEdit[fieldName] = e.target.value;
-
-        this.setState({
-            brewery: breweryToEdit
-        })
-    }
-
+class BreweryEdit extends React.Component {
     saveBrewery() {
-        console.log(this.state.brewery);
-        const { actions } = this.props;
+        // console.log(this.state.brewery);
+        const { actions, editingBrewery } = this.props;
         // TODO(fj): GraphQL CRUD mutations
-        if (this.state.brewery._id) {
+        if (editingBrewery._id) {
             actions.graphqlMutation(`
                 mutation Mutation {
                     updateBrewery (
-                        id: "${this.state.brewery._id}",
-                        name: "${this.state.brewery.name}",
-                        description: "${this.state.brewery.description}",
-                        established: ${this.state.brewery.established},
-                        website: "${this.state.brewery.website}",
-                        country: "${this.state.brewery.country}") {
+                        id: "${editingBrewery._id}",
+                        name: "${editingBrewery.name}",
+                        description: "${editingBrewery.description}",
+                        established: ${editingBrewery.established},
+                        website: "${editingBrewery.website}",
+                        logoUrl: "${editingBrewery.logoUrl}",
+                        country: "${editingBrewery.country}") {
                     
                         _id,
                         name,
                         description,
                         established,
                         website,
+                        logoUrl,
                         country
                     }
                 }
@@ -68,17 +40,19 @@ class BreweriesTable extends React.Component {
             actions.graphqlMutation(`
                 mutation Mutation {
                     addBrewery (
-                        name: "${this.state.brewery.name}",
-                        description: "${this.state.brewery.description}",
-                        established: ${this.state.brewery.established},
-                        website: "${this.state.brewery.website}",
-                        country: "${this.state.brewery.country}") {
+                        name: "${editingBrewery.name}",
+                        description: "${editingBrewery.description}",
+                        established: ${editingBrewery.established},
+                        website: "${editingBrewery.website}",
+                        logoUrl: "${editingBrewery.logoUrl}",
+                        country: "${editingBrewery.country}") {
                     
                         _id,
                         name,
                         description,
                         established,
                         website,
+                        logoUrl,
                         country
                     }
                 }
@@ -94,56 +68,47 @@ class BreweriesTable extends React.Component {
     }
 
     render() {
-        const { brewery, actions } = this.props;
-        const breweryToEdit = this.state.brewery;
+        const { editingBrewery, actions } = this.props;
+        console.log(editingBrewery);
 
         return (
             <form className="brewery-edit">
-                <FormGroup>
-                    <ControlLabel>Name:</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={breweryToEdit.name}
-                        placeholder="eg. Brewdog"
-                        onChange={e => this.updateField('name', e)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Description:</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={breweryToEdit.description}
-                        placeholder="eg. We make awesome Beer!"
-                        onChange={e => this.updateField('description', e)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Established:</ControlLabel>
-                    <FormControl
-                        type="number"
-                        value={breweryToEdit.established}
-                        placeholder="eg. 2000"
-                        onChange={e => this.updateField('established', e)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Website:</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={breweryToEdit.website}
-                        placeholder="eg. www.beer.com"
-                        onChange={e => this.updateField('website', e)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Country:</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={breweryToEdit.country}
-                        placeholder="eg. USA"
-                        onChange={e => this.updateField('country', e)}
-                    />
-                </FormGroup>
+                <SmartInput
+                    type="text"
+                    placeholder="eg. Brewdog"
+                    label="Name:"
+                    field="editingBrewery.name"
+                    value={editingBrewery.name}
+                    actions={actions}/>
+                <SmartInput
+                    type="text"
+                    placeholder="eg. We make awesome IPA!"
+                    label="Description:"
+                    field="editingBrewery.description"
+                    value={editingBrewery.description}
+                    actions={actions}/>
+                <SmartInput
+                    type="number"
+                    placeholder="eg. 2010"
+                    label="Established:"
+                    field="editingBrewery.established"
+                    value={editingBrewery.established}
+                    actions={actions}/>
+                <SmartInput
+                    type="text"
+                    placeholder="eg. www.beer.org"
+                    label="Website:"
+                    field="editingBrewery.website"
+                    value={editingBrewery.website}
+                    actions={actions}/>
+                <SmartInput
+                    type="text"
+                    placeholder="eg. Japan"
+                    label="Country:"
+                    field="editingBrewery.country"
+                    value={editingBrewery.country}
+                    actions={actions}/>
+
                 <Button onClick={() => this.saveBrewery()} ><FaFloppyO /> Save</Button>
                 <Button onClick={() => this.cancel()} ><FaTimes /> Cancel</Button>
             </form>
@@ -152,9 +117,9 @@ class BreweriesTable extends React.Component {
 }
 
 // Set component propTypes
-BreweriesTable.propTypes = {
+BreweryEdit.propTypes = {
     actions: PropTypes.object,
     breweries: PropTypes.array
 };
 
-export default BreweriesTable;
+export default BreweryEdit;
